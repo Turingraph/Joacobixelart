70f1bf5765c267cbf59cf4395391c3e5021d7efc
Turingraph <nemocheche@gmail.com>
2025-12-20 13:57:54 +0700

    Simplify b_rgb_grid.tsx

--- front_end/src/organism/button/b_rgb_grid.tsx
+++ front_end/src/organism/button/b_rgb_grid.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import * as a from "../../atom/type/alias";
 import B_LOGO from "../../molecule/button/b_logo";
 import b_x from "../../asset/b_items/b_x.png"
-import "./b_rgb_grid.css"
 
 export function B_RGB_GRID({
 	mode = false,
@@ -26,7 +25,6 @@ export function B_RGB_GRID({
 		width:"45px", 
 		height:"45px", 
 	}}
-		className="CONTAINER_B_RGB_GRID"
 	>
 		<div style={{
 			margin:"0px", 
@@ -34,34 +32,9 @@ export function B_RGB_GRID({
 			width:"100%",
 			height:"100%"
 		}}
-			className="B_RGB_GRID"
-			draggable={mode!}
 		>
 		{jsx_x}
 		<p style={{margin:"0px"}}>{title}</p>
 		</div>
 	</div>
 }
-
-const draggables = document.querySelectorAll('.B_RGB_GRID')
-const containers = document.querySelectorAll('.CONTAINER_B_RGB_GRID')
-
-draggables.forEach(draggable => {
-	draggable.addEventListener('dragstart', () => {
-		draggable.classList.add('DRAGGED_B_RGB_GRID')
-	})
-	draggable.addEventListener('dragend', () => {
-		draggable.classList.remove('DRAGGED_B_RGB_GRID')
-	})
-})
-
-containers.forEach(container => {
-	container.addEventListener('dragover', e => {
-		e.preventDefault()
-		const draggable = document.querySelector('.DRAGGED_B_RGB_GRID')
-		if (draggable !== null)
-		{
-			container.appendChild(draggable)
-		}
-	})
-})
