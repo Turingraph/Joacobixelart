3f93dc4f5c363d4037bf7a58c3a0a233a0f87519
Turingraph <nemocheche@gmail.com>
2025-12-23 14:58:00 +0700

    Simplify test operation

--- front_end/src/page/utils/arr.tsx
+++ front_end/src/page/utils/arr.tsx
@@ -13,75 +13,66 @@ import { t_B_LOGO } from "../../molecule/button/b_logo";
 import { t_B_STR } from "../../molecule/button/b_str";
 import { CSS_TEST_DIV } from "../../molecule/html/main_css";
 import { ui_with_title } from "../../organism/html/multi_modes_page";
-// import { t_div_with_title } from "../../ui/html/multi_modes_div";
-
-const f_func = () => {
-    alert("Nujabes");
-}
-
-const f_func_input = (input:string) => {
-    alert(input);
-}
 
 export const ARR_B_PAINTS:t_B_LOGO[] = [
 	{
 		title:"Pen" as a.t_str_hover,
 		logo:b_pen as a.t_logo,
-		func:(()=>{f_func_input("Pen")}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Eraser" as a.t_str_hover,
 		logo:b_eraser as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Mittor Pen" as a.t_str_hover,
 		logo:b_mirror_pen as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Bucket" as a.t_str_hover,
 		logo:b_bucket as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Rectangle" as a.t_str_hover,
 		logo:b_rectangle as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Move" as a.t_str_hover,
 		logo:b_move as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Rectangle select" as a.t_str_hover,
 		logo:b_rectangle_select as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"Color Picker" as a.t_str_hover,
 		logo:b_rgb_picker as a.t_logo,
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	}
 ]
 
 export const ARR_B_SAVE:t_B_STR[] = [
 	{
 		title:"save as",
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 	{
 		title:"import image",
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},	
 	{
 		title:"export project",
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},	
 	{
 		title:"create new project",
-		func:(()=>{f_func()}) as a.t_func
+		func:(()=>{}) as a.t_func
 	},
 ]
 
