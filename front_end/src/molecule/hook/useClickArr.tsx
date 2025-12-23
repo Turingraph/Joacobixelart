e647d3a0eb4771b92d096550d672b635e7522e7e
Turingraph <nemocheche@gmail.com>
2025-12-23 14:57:38 +0700

    Add new hook for useReducer on nested array.

--- front_end/src/molecule/hook/useClickArr.tsx
+++ front_end/src/molecule/hook/useClickArr.tsx
@@ -1,5 +1,7 @@
 import { useLayoutEffect, RefObject } from "react";
-import { t_setss_arr } from "../../atom/arr/act_arr";
+import { t_setss_arr, t_use_arr } from "../../atom/arr/act_arr";
+import { t_2d_arr } from "../../atom/arr/type";
+import { edit } from "../../atom/arr/function";
 
 // https://www.w3schools.com/react/react_customhooks.asp
 
@@ -44,3 +46,28 @@ export const useClickPushArr =
 		}
 	})
 };
+
+// export const useClickEditArr2D = 
+// 	<t extends {id:number}>(
+// 		Ref_ID: RefObject<[number, number] | undefined>,
+// 		SS_Arr: t_use_arr<t_2d_arr<t>, "id" | "arr">, 
+// 		input: t) => {
+// 	useLayoutEffect(()=>{
+// 		if (Ref_ID.current !== undefined)
+// 		{
+// 			let sub_arr = [...SS_Arr.ss]
+// 				.filter((item)=>Ref_ID.current !== undefined && item.id === Ref_ID.current[0])[0]
+// 			sub_arr.arr = edit(
+// 				sub_arr.arr,
+// 				sub_arr.arr.filter((jtem)=>Ref_ID.current !== undefined && jtem.id === Ref_ID.current[1])[0].id,
+// 				input
+// 			)
+// 			SS_Arr.setss({
+// 				type:"EDIT",
+// 				id:Ref_ID.current[0],
+// 				input:sub_arr
+// 			})
+// 			Ref_ID.current = undefined
+// 		}
+// 	})
+// }
